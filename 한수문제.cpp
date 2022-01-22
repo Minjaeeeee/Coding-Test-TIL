126db3c161b314c9bbd8d0647d73531ed730c47e
Minjaeeeee <94120988+Minjaeeeee@users.noreply.github.com>
2022-01-22 18:31:53 +0900

    Update 한수문제.cpp

--- 한수문제.cpp
+++ 한수문제.cpp
@@ -4,61 +4,48 @@
 int main(void)
 {
     int hund=0,ten=0,one=0; 
-    int count=99; 
+    int count=99; //1부터 99까지는 전부 한수이기 때문이다. 
     int num; 
     int minus1=0,minus2=0;
 
     scanf("%d",&num); 
     
-    if(num<100) 
-	{
-		printf("%d",num);
-	}
-
-
-
-    for(int i=100; i<=num; i++)
-    {
-       
-        if(i>=100 && i<1000)
-        {
-            hund = i/100; 
-            ten = (i%100)/10;
-            one = (i%10)/1;
-			
-			minus1 = hund-ten;
-			minus2 = ten-one;
-			
-
-            if(hund-ten<0 || ten-one<0)
-            {
-                if(hund-ten<0)
-                {
-                    minus1 = -(hund-ten);
-                }
-
-                else if(ten - one <0)
-                {
-                	minus2 = -(ten-one);
-				}
-				
-				else continue;
-            }
-
-
-            if(minus1==minus2) count++;
-        }
-        
-        if(i==num) 
-        {
-        	printf("%d",count);
-        	break;
-		}
-    }
-
+    //이쪽은 정상출력된다. 
     
-    return 0;
+    if(num<100) //100이하는 전부 한수이므로 100이하는 전부 출력하기 
+	{
+		printf("%d",num); 
+		return 0;
+	}
+	
+	else if(num==1000)
+	{
+		printf("%d",144);
+		return 0;
+	}
+	
+	else
+	{
+		for(int i=100; i<=num; i++) //210이면 100부터 210까지 돌린다. 
+		{
+			hund = i/100; 
+			ten = (i%100)/10;
+			one = (i%10)/1;
+			
+			minus1 = hund - ten;
+			minus2 = ten - one;
+			
+			if(minus1 == minus2) 
+			{
+				count++; 
+			}
+		}
+	}
+	printf("%d",count);
+	return 0;
 }
+
+
 
 
 
